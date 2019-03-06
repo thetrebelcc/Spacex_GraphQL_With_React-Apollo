1cc8a0c24042bb69d3835bbc8fd4acd93d0b2619
Fabian Anguiano <fabiananguiano@gmail.com>
2019-03-05 20:02:26 -0600

    server trouble shooting

--- server.js
+++ server.js
@@ -1,29 +1,60 @@
+// const express = require('express');
+// const  graphqlHTTP = require('express-graphql');
+// const schema = require('./schema');
+// const cors = require('cors');
+// const path = require('path');
+
+// const app = express();
+
+// app.use(cors());
+
+// app.use('/graphql',
+//  graphqlHTTP({
+//     schema,
+//     graphiql: true
+// }));
+
+
+
+// app.use(express.static('public'));
+
+// app.get('*', (req,res) => {
+//     res.sendFile(path.resolve(__dirname,'public','index.html'));
+// });
+
+
+
+// const PORT = process.env.port || 5000;
+
+// app.listen(PORT, () => console.log(`sever started on port ${PORT}`));
+
+
+
 const express = require('express');
-const  graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
+const graphqlHTTP = require('express-graphql');
 const cors = require('cors');
+const schema = require('./schema');
 const path = require('path');
 
 const app = express();
 
+// Allow cross-origin
 app.use(cors());
 
-app.use('/graphql',
- graphqlHTTP({
-    schema,
-    graphiql: true
-}));
-
-
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true
+    })
+);
 
 app.use(express.static('public'));
 
-app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'public','index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
+const PORT = process.env.PORT || 5000;
 
-
-const PORT = process.env.port || 5000;
-
-app.listen(PORT, () => console.log(`sever started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
