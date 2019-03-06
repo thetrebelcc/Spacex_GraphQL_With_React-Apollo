01eef585989f3711e1f0079399765085dd9cd6c2
Fabian Anguiano <fabiananguiano@gmail.com>
2019-03-05 19:38:11 -0600

    first verison done

---
+++ public/precache-manifest.6c062e8cc92447bb2c10e8876aa4e25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "25c91854adfe55232d8b",
+    "url": "/static/js/main.25c91854.chunk.js"
+  },
+  {
+    "revision": "35b720f79b0f8bb89d48",
+    "url": "/static/js/2.35b720f7.chunk.js"
+  },
+  {
+    "revision": "25c91854adfe55232d8b",
+    "url": "/static/css/main.a26afbcf.chunk.css"
+  },
+  {
+    "revision": "89985a5f523623f98ef3eae5480930fa",
+    "url": "/index.html"
+  }
+];
